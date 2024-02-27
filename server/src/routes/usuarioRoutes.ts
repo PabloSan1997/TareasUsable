386e5764a773da15a16778b25d620e80ec8366d3
PabloSan1997 <jos_pab_997@hotmali.com>
2024-02-27 17:18:43 -0600

    Se corrigio revicion de los datos para agregar y eliminar usuario

--- server/src/routes/usuarioRoutes.ts
+++ server/src/routes/usuarioRoutes.ts
@@ -1,14 +1,13 @@
 import express from 'express';
 import { UsuarioController } from '../controller/UsuarioController';
 import { joiHandle } from '../middlewares/joiHandle';
-import { addTareaJoi } from '../middlewares/schema/tareaJoi';
-import { loginJoi } from '../middlewares/schema/usuarioJoi';
+import { addUserJoi, loginJoi, selecUserJoi } from '../middlewares/schema/usuarioJoi';
 
 export const usuarioRoute = express.Router();
 const controller = new UsuarioController();
 
 usuarioRoute.post('/addnewuser',
-	joiHandle(addTareaJoi, 'body'),
+	joiHandle(addUserJoi, 'body'),
 	controller.addNewUser);
 
 usuarioRoute.post('/login',
@@ -16,5 +15,5 @@ usuarioRoute.post('/login',
 	controller.login);
 
 usuarioRoute.delete('/:id_usuario',
-	joiHandle(loginJoi, 'params'),
+	joiHandle(selecUserJoi, 'params'),
 	controller.deleteUser);
