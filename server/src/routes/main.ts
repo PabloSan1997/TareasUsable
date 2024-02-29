75bbc9dbb8b6276cbd4e39d06db9aadbd7ea3592
PabloSan1997 <jos_pab_997@hotmali.com>
2024-02-28 20:27:13 -0600

    Se agrego parte static

--- server/src/routes/main.ts
+++ server/src/routes/main.ts
@@ -7,4 +7,5 @@ export function createApi(app: Express){
 	app.use('/api/v1', mainRoute);
 	mainRoute.use('/usuario', usuarioRoute);
 	mainRoute.use('/tareas', tareasRoutes);
+	app.use('/', express.static('src/static'));
 }
