53c13f0c0f3060fb2821c6969d56b7d632ba8016
PabloSan1997 <jos_pab_997@hotmali.com>
2024-02-29 13:27:29 -0600

    Se agregaron bases para produccion

--- page/src/api/apiConfig.ts
+++ page/src/api/apiConfig.ts
@@ -1,8 +1,9 @@
 
 
+const base_url = import.meta.env.DEV?'http://localhost:3005': import.meta.env.VITE_URL_FETCH;
 export const apiConfig = {
-    url_api_tareas: 'http://localhost:3005/api/v1/tareas',
-    url_api_usuario: 'http://localhost:3005/api/v1/usuario',
+    url_api_tareas: `${base_url}/api/v1/tareas`,
+    url_api_usuario: `${base_url}/api/v1/usuario`,
     headersJsonAuth:(authorization:string)=>{
         return {
             'Content-Type':'application/json',
