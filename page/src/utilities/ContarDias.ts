a62af76a79cf3e83176b7d4dbbb59861cb39b145
PabloSan1997 <jos_pab_997@hotmali.com>
2024-02-28 20:01:19 -0600

    Se modifico mensajes de tareas

--- page/src/utilities/ContarDias.ts
+++ page/src/utilities/ContarDias.ts
@@ -5,6 +5,7 @@ export function contarDias(data1: string): { conteo: string, urgencia: boolean }
     const fechaActual = new Date();
     const calcular = fechafinal.getTime() - fechaActual.getTime();
     const diferenciaEnDias: number = Math.floor(calcular / (1000 * 3600 * 24));
-    if (diferenciaEnDias < 5) return { conteo: `Quedan ${diferenciaEnDias} dias`, urgencia: true }
-    return { conteo: '', urgencia: false }
+    if (diferenciaEnDias < 5 && diferenciaEnDias >= 0) return { conteo: `Quedan ${diferenciaEnDias} dias`, urgencia: true }
+    else if (diferenciaEnDias < 0) return { conteo: 'Ya se te terminó el tiempo', urgencia: true }
+    else return { conteo: '', urgencia: false }
 }
